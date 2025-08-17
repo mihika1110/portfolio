da4b9e9ce413d1ebafd4e96c24c8b580d0cb0926
mihika1110 <mihikas02@gmail.com>
2025-08-17 22:17:12 +0530

    about changes

--- components/About.tsx
+++ components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <section id="about" className="w-full py-16 flex flex-col items-center justify-center scroll-mt-24">
       <h2 className="heading mb-6">Mihika</h2>
       <p className="max-w-2xl text-center text-lg text-gray-300">
-        I'm Mihika - a developer, designer, and problem solver. Whether it's an intuitive mobile app, a powerful backend, or an AI-driven game, I love creating solutions that blend creativity and logic. Currently pursuing B.Tech in CSE at IIT Patna, I've worked on diverse projects in Android, AI, databases, and web development. Dive into my work and explore my journey!
+        I'm Mihika - a developer, designer, and problem solver. Whether it's an intuitive mobile app, a powerful backend, or an AI-driven game, I love creating solutions that blend creativity and logic. Currently pursuing B.Tech in CSE at IIT Patna, I've worked on diverse projects in Android, AI, Database, and Web Development. Dive into my work and explore my journey!
       </p>
     </section>
   );
